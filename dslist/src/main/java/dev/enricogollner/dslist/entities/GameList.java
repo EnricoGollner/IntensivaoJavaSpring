6f72cb8aefedb0450d49e946ef75dbd3eb3c1b14
Enrico Gollner <enricogollner.tech@gmail.com>
2023-07-11 22:23:19 -0300

    Implementando o modelo de domínio

---
+++ dslist/src/main/java/dev/enricogollner/dslist/entities/GameList.java
@@ -0,0 +1,13 @@
+package dev.enricogollner.dslist.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_game_list")
+public class GameList {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+}
